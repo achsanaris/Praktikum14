f48aadeb8620f855a0d6485932b5128709df4d2d
achsanaris <Achsanaris05@gmail.com>
2021-02-06 21:32:38 +0700

    prepare project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Quotes_Apps
+![Android CI](https://github.com/irvandandung/Quotes_Apps/workflows/Android%20CI/badge.svg)
---
+++ app/src/main/java/project/achsan/quotesapps/MainActivity.kt
@@ -0,0 +1,56 @@
+package project.achsan.quotesapps
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import project.achsan.quotesapps.models.Token
+import project.achsan.quotesapps.ui.LoginActivity
+import project.achsan.quotesapps.utils.TokenPref
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var tokenPref: TokenPref
+    private lateinit var token: Token
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        tokenPref = TokenPref(this)
+        token = tokenPref.getToken()
+        if (TextUtils.isEmpty(token.token)) {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_form, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_logout -> {
+                tokenPref.removeToken()
+                val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
